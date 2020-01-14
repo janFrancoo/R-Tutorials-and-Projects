6aa795614b8c3d79642692c9c95f965aa5816633
Eren Sırakaya <45636602+janFrancoo@users.noreply.github.com>
2020-01-14 16:46:56 +0300

    Update Matrices.R

--- Matrices.R
+++ Matrices.R
@@ -18,3 +18,17 @@ mat <- matrix(1:50, nrow = 10, byrow = TRUE)
 mat[1,]
 mat[,1]
 mat[1:5, 1:3]
+
+A <- c(1, 2, 3)
+B <- c(4, 5, 6)
+C <- rbind(A, B)
+
+mat <- matrix(1:9, byrow = TRUE, nrow = 3)
+is.matrix(mat)
+
+mat <- matrix(1:25, byrow = TRUE, nrow = 5)
+mat[2:3, 2:3]
+mat[4:5, 4:5]
+sum(mat)
+
+mat <- matrix(runif(20, max = 100), nrow = 4)
